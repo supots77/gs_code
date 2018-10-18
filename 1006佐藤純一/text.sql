29f230f1152aa5429fdef2c2bffeb1a7242f1ddb
Unknown <supots77@gmail.com>
2018-10-18 16:23:09 +0900

    Revert "1006佐藤純一"
    This reverts commit 1421a158dd3a7e01714b366d825086d971c5df35.

--- 1006佐藤純一/text.sql
+++
@@ -1,6 +0,0 @@
- UPDATE gs_an_table SET name='sato',email='aaaa@ywtww',naiyou ='atarashuii naiyou' WHERE id =1
-
-
- UPDATE gs_an_table SET name=:name,email=:email,naiyou =:naiyou WHERE id=:id
-
-SELECT * FROM gs_an_table 
